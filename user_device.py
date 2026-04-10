697ce3ec7ef77de00342d7dd6201cf4b2f590f23
aarsinh <148481261+aarsinh@users.noreply.github.com>
2026-04-10 13:58:50 +0530

    basic functionalities

--- user_device.py
+++ user_device.py
@@ -2,7 +2,10 @@ def make_transaction():
     '''
     1. Scan QR → get encrypted FID
     2. Enter: VMID, PIN, Amount
-    3. Send to kiosk
+    3. Hash PIN
+    4. Encrypt the data
+    5. Send to kiosk
+
+    The details should be RSA encrypted
     '''
-    
     pass
