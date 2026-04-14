a31d738a43b0f94ce5a4eb2b8e18c62729ec9648
aarsinh <148481261+aarsinh@users.noreply.github.com>
2026-04-14 11:42:46 +0530

    rsa encryption

--- charging_kiosk.py
+++ charging_kiosk.py
@@ -1,10 +1,11 @@
-
 import qrcode
 import time
 from ascon import encrypt, decrypt
 import os
 import json
 import base64
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 class Kiosk:
     def __init__(self, grid_authority):
@@ -12,6 +13,10 @@ class Kiosk:
         self.fid = None
         self.current_vfid = "abcd"
         self.current_qr_code = None
+        self.key = os.urandom(16)
+
+        with open("private.pem", "rb") as f:
+            self.private_key = RSA.import_key(f.read())
 
     def receive_fid(self, fid: str):
         self.fid = fid
@@ -32,6 +37,7 @@ class Kiosk:
 
         vmid and pin are rsa-encrypted
         '''
+        received_vfid = request["vfid"]
 
     def lwc_encrypt(self, fid: str):
         payload = {
@@ -39,23 +45,35 @@ class Kiosk:
                 "ts": int(time.time())
         }
 
-        key = os.urandom(16)
         nonce = os.urandom(16)
         plaintext = json.dumps(payload).encode()
 
-        ciphertext = encrypt(key=key, nonce=nonce, associateddata=None, plaintext=plaintext, variant="Ascon-128")
+        ciphertext = encrypt(key=self.key, nonce=nonce, associateddata=None, plaintext=plaintext, variant="Ascon-128")
 
         return {
                 "nonce": nonce,
                 "ciphertext": ciphertext
         }
 
-    def lwc_decrypt(self, packet):
-        pass
+    def lwc_decrypt(self, vfid: str):
+        nonce_b64, ct_b64 = vfid.split(".")
+        nonce = base64.urlsafe_b64decode(nonce_b64)
+        ciphertext = base64.urlsafe_b64decode(ct_b64)
+        plaintext = decrypt(key=self.key, nonce=nonce, associateddata=None, ciphertext=ciphertext, variant="Ascon-128")
+        if plaintext is None:
+            raise ValueError("Decryption of vfid failed on kiosk end")
+
+        payload = json.loads(plaintext.decode())
+        return payload['fid']
 
     def make_vfid(self, packet):
         nonce_b64 = base64.urlsafe_b64encode(packet['nonce']).decode()
-        ct_b64 = base64.urlsafe_b64encode(packet['cipertext']).decode()
+        ct_b64 = base64.urlsafe_b64encode(packet['ciphertext']).decode()
 
         return f"{nonce_b64}.{ct_b64}"
+
+    def rsa_decrypt(self, ct):
+        cipher = PKCS1_OAEP.new(self.private_key)
+        cipher.decrypt(ct).decode()
+
 
